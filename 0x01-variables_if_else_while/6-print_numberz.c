76638ab4845eb960508c4496c9f630f11274a3d2
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2023-09-08 13:28:49 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry Point.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	putchar(48);
+
+	putchar(48 + 1);
+	putchar(48 + 2);
+	putchar(48 + 3);
+	putchar(48 + 4);
+	putchar(48 + 5);
+	putchar(48 + 6);
+	putchar(48 + 7);
+	putchar(48 + 8);
+	putchar(48 + 9);
+
+	 putchar('\n');
+	return (0);
+}
