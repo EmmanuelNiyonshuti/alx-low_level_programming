bfa2ac1b30838110cf093b56484cbdbeb812ca2b
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2023-09-12 16:50:16 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -6,13 +6,7 @@
  * Return: Always 0.
  */
 int main(void)
-{	
-	int r;
-
-    print_last_digit(98);
-    print_last_digit(0);
-    r = print_last_digit(-20);
-    _putchar('0' + r);
-    _putchar('\n');
-    return (0);
+{
+	jack_bauer();
+         return (0);
 }
