594c14fc9bab41765149f68bacb703a3395dcd3f
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2023-09-19 12:44:52 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-main.c
+++ 0x05-pointers_arrays_strings/100-main.c
@@ -1,30 +1,19 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- * main - check the code
+ * main - Entry point
  *
- * Return: Always 0.
- */
+ * Description: This is the main function where the _atoi function is
+ *              tested by converting a string to an integer and printing
+ *              the result.
+ *
+ * Return: Always 0 (Success)
+*/
 int main(void)
 {
-	int nb;
+	char str[] = "   -12345abc";
+	int result = _atoi(str);
 
-	nb = _atoi("98");
-	printf("%d\n", nb);
-	nb = _atoi("-402");
-	printf("%d\n", nb);
-	nb = _atoi("          ------++++++-----+++++--98");
-	printf("%d\n", nb);
-	nb = _atoi("214748364");
-	printf("%d\n", nb);
-	nb = _atoi("0");
-	printf("%d\n", nb);
-	nb = _atoi("Suite 402");
-	printf("%d\n", nb);
-	nb = _atoi(" + + - -98 Battery Street; San Francisco, CA 94111 - USA ");
-	printf("%d\n", nb);
-	nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
-	printf("%d\n", nb);
+	printf("Result: %d\n", result);
 	return (0);
 }
