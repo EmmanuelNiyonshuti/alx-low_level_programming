d23297654f03d0abfecb328835d72465a2e1fb6e
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2023-09-19 13:40:11 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,18 +11,19 @@ int _atoi(char *s)
 	int i = 0;
 	int sign = 1;
 	int result = 0;
+	int found_digit = 0;
 
-	/* Handle leading '+' or '-' signs */
-	if (s[0] == '-' || s[0] == '+')
+	while (s[i] != '\0')
 	{
-	if (s[0] == '-')
-	sign = -1;
-	i++;
-	}
-
-	while (s[i] != '\0' && s[i] >= '0' && s[i] <= '9')
+	if (s[i] == '-')
+	sign *= -1;
+	else if (s[i] >= '0' && s[i] <= '9')
 	{
 	result = result * 10 + (s[i] - '0');
+	found_digit = 1;
+	}
+	else if (found_digit)
+	break;  /* Stop if non-digit encountered after numbers */
 	i++;
 	}
 
