4f4557229304eff1df2978a2ff8465be446acd86
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2023-09-19 14:11:13 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -19,13 +19,24 @@ int _atoi(char *s)
 	sign *= -1;
 	else if (s[i] >= '0' && s[i] <= '9')
 	{
-	result = result * 10 + (s[i] - '0');
+	int digit = s[i] - '0';
+
+  
+	if (result > 214748364 || (result == 214748364 && digit > 7))
+	{
+	if (sign == 1)
+	return 2147483647;
+	else
+	return -2147483648;
+	}
+
+	result = result * 10 + digit;
 	found_digit = 1;
 	}
 	else if (found_digit)
-	break;  /* Stop if non-digit encountered after numbers */
+	break;  /* Stop if non-digit encountered after digits */
 	i++;
 	}
 
-	return (result * sign);
+	return result * sign;
 }
