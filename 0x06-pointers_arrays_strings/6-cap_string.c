c12abc2b6d07622b77900eaced9c893ae33df3a2
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2023-09-20 23:02:41 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,54 +1,36 @@
-#include "main.h"
-
 /**
- * is_separator - Checks if a character is a separator.
- * @c: The character to check.
- *
- * Return: 1 if the character is a separator, 0 otherwise.
- */
-int is_separator(char *c)
-{
-	char separators[] = " \t\n,;.!?\"(){}";
-	int i;
-
-	for (i = 0; separators[i] != '\0'; i++)
-	{
-		if (*c == separators[i])
-			return (1);
-	}
-
-	return (0);
-}
-
-/**
- * cap_string - Capitalizes all words of a string.
- * @str: The string to be capitalized.
+ * cap_string - Capitalizes all words of a string
+ * @str: The input string
  *
  * Return: A pointer to the modified string.
  */
 char *cap_string(char *str)
 {
-	int i = 0;
+	int count = 0;
 
-	while (str[i] != '\0')
+	while (str[count])
 	{
-		if (i == 0 || is_separator(&str[i - 1]))
-		{
-			if (str[i] >= 'a' && str[i] <= 'z')
-			{
-				str[i] = str[i] - 32;
-			}
-		}
-		else
-		{
-			if (str[i] >= 'A' && str[i] <= 'Z')
-			{
-				str[i] = str[i] + 32;
-			}
-		}
+		while (!(str[count] >= 'a' && str[count] <= 'z'))
+			count++;
 
-		i++;
+		if (str[count - 1] == ' ' ||
+			str[count - 1] == '\t' ||
+			str[count - 1] == '\n' ||
+			str[count - 1] == ',' ||
+			str[count - 1] == ';' ||
+			str[count - 1] == '.' ||
+			str[count - 1] == '!' ||
+			str[count - 1] == '?' ||
+			str[count - 1] == '"' ||
+			str[count - 1] == '(' ||
+			str[count - 1] == ')' ||
+			str[count - 1] == '{' ||
+			str[count - 1] == '}' ||
+			count == 0)
+		{
+			str[count] -= 32;
+		}
+		count++;
 	}
-
 	return (str);
 }
