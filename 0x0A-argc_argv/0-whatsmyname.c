41329d33f6f948811432e01e85d401b53823af75
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2023-09-29 13:00:22 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * @argc: Number of command-line arguments
+ * @argv: Array containing the command-line arguments
+ *
+ * Return: Always 0 (Success)
+*/
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+	}
+	else
+	{
+		printf("Program name not available\n");
+	}
+	return (0);
+}
