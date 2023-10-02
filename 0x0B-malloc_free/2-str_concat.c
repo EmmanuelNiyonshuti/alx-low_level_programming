52d3d921b8d46994fe252d03795f8e64fab14724
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2023-10-02 14:32:29 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,11 +16,11 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 	{
-	s1 = " ";
+	s1 = "";
 	}
 	if (s2 == NULL)
 	{
-	s2 = " ";
+	s2 = "";
 	}
 	while (s1[len_s1] != '\0')
 	{
@@ -31,6 +31,7 @@ char *str_concat(char *s1, char *s2)
 	len_s2++;
 	}
 	concatenated = (char *)malloc((len_s1 + len_s2 + 1) * sizeof(char));
+
 	if (concatenated == NULL)
 	{
 	return (NULL);
@@ -46,4 +47,3 @@ char *str_concat(char *s1, char *s2)
 	concatenated[i] = '\0';
 	return (concatenated);
 }
-
