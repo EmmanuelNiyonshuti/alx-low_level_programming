70f5845237b21dff2d1ccbcfbd804c82f86396b5
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2023-10-05 16:05:50 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,32 +19,32 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *concat;
 
 	if (s1 == NULL)
-	{
+
 		s1 = " ";
-	}
+
 	if (s2 == NULL)
-	{
+
 		s2 = " ";
-	}
+
 	while (s1[s1_len] != '\0')
-	{
+
 		s1_len++;
-	}
+
 	while (s2[s2_len] != '\0')
-	{
+
 		s2_len++;
-	}
+
 
 	concat = malloc((s1_len + n + 1) * sizeof(char));
 
 	for (i = 0; i < s1_len; i++)
-	{
+
 		concat[i] = s1[i];
-	}
+
 	for (j = 0; j < n && j < s2_len; j++)
-	{
+
 		concat[i + j] = s2[j];
-	}
+
 	concat[i + j] = '\0';
 	return (concat);
 }
