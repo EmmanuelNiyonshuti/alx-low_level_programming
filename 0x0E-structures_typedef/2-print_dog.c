fae2264217701e11be4e5fdd3fa51b9c40c3c972
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2023-10-10 07:34:23 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,5 +15,5 @@ void print_dog(struct dog *d)
 	}
 	printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
 	printf("Age: %f\n", d->age);
-	printf("owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
+	printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
 }
