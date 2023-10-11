f8f41510338b5806f90ea362bf9e64090b0e9e86
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2023-10-11 13:38:38 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,13 +1,9 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-#ifndef _SIZE_T_DEFINED
-#define _SIZE_T_DEFINED
-typedef unsigned long size_t;
-#endif
-
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+typedef unsigned long size_t;
 void array_iterator(int *array, size_t size, void (*action)(int));
 
 
