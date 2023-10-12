b88d7d32bce61c0b6e61d8e10b17b914cd64d7a1
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2023-10-12 10:01:17 -0700

    0-sum

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,21 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+
+
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /*VARIADIC_FUNCTIOONS_H*/
