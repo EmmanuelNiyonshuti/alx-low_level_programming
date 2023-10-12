4f47e97077fed08a12bcd4f0fbf374d7eb5e21f1
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2023-10-12 12:59:15 -0700

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -27,8 +27,11 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			printf("(nil)");
 		}
+		if (separator != NULL && i < n -1)
+		{
+			printf("%s", separator);
+		}
 	}
 	printf("\n");
 	va_end(strArgs);
 }
-
