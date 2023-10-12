7f0618cd8cfffa75cffbd676d6ba98d2b5f1a8c3
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2023-10-12 05:21:10 -0700

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -14,19 +14,17 @@ void printOpcodes(int numBytes)
 
 	if (numBytes < 0)
 	{
-		fprintf(stderr, "Error\n");
+		printf("Error\n");
 		exit(2);
 	}
 
 	printf("Opcodes of main function:\n");
 
-	void *mainAddr;
-
-	asm("lea (%%rip), %0" : "=r"(mainAddr));
+	void (*printOpcodesAddr)(int) = &printOpcodes;
 
 	for (i = 0; i < numBytes; ++i)
 	{
-		printf("%02x", *((unsigned char *)mainAddr + i));
+		printf("%02x", *((unsigned char *)printOpcodesAddr + i));
 		if (i < numBytes - 1)
 		{
 			printf(" ");
@@ -49,7 +47,7 @@ int main(int argc, char *argv[])
 {
 	if (argc != 2)
 	{
-		fprintf(stderr, "Error\n");
+		printf("Error\n");
 		return (1);
 	}
 	int numBytes = atoi(argv[1]);
