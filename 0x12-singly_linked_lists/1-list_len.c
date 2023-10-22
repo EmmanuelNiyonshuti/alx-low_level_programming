d195068cc4a84cc41278ce31ec82d66c6d329a7c
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2023-10-22 00:35:17 -0700

    modified elementCount

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -8,14 +8,14 @@
  */
 size_t list_len(const list_t *h)
 {
-	size_t num = 0;
+	size_t elementCount = 0;
 
 	while (h != NULL)
 	{
 
-		num++;
+		elementCount++;
 		h = h->next;
 	}
 
-	return (num);
+	return (elementCount);
 }
