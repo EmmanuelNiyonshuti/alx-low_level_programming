a8f4692e9c4b88769cfae1726e03a51cd60a1bd3
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2023-10-22 22:22:37 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdio.h>
+
 /**
  *struct listint_s - singly linked list
  * @n: integer
@@ -15,7 +17,7 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
-
+size_t print_listint(const listint_t *h);
 
 
 
