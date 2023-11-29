38738bb9fbab64a353f6b30c1f3b052c8d9b49b7
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2023-11-29 09:28:12 -0800

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,13 +13,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	char *buffer;
-	ssize_t bytesRead, bytesPrint;
+	ssize_t bytesRead, bytesWritten;
+	ssize_t TotalBytesWritten = 0;
 
 	if (filename == NULL)
 	{
 		return (0);
 	}
-
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
@@ -31,24 +31,28 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fd);
 		return (0);
 	}
+
 	bytesRead = read(fd, buffer, letters);
+	while (bytesRead > 0)
+	{
+		bytesWritten = write(1, buffer, bytesRead);
+		if (bytesWritten == -1)
+		{
+			free(buffer);
+			close(fd);
+			return (0);
+		}
+		TotalBytesWritten += bytesRead;
+		bytesRead = read(fd, buffer, letters);
+	}
 	if (bytesRead == -1)
 	{
-		close(fd);
 		free(buffer);
+		close(fd);
 		return (0);
 	}
-	buffer[bytesRead] = '\0';
-
-	bytesPrint = write(1, buffer, letters);
-	if (bytesPrint == -1)
-	{
-		close(fd);
-		free(buffer);
-		return (0);
-	}
-	close(fd);
 	free(buffer);
+	close(fd);
 
-	return (bytesRead);
+	return (TotalBytesWritten);
 }
