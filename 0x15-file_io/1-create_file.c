3ccafa9cbe1d15fb541eeb092ab5c618ee8c9d14
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2023-11-29 10:27:10 -0800

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	int bytesWrite;
+	int bytesToWrite;
 
 	if (filename == NULL)
 	{
@@ -18,14 +18,14 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT | S_IRUSR | S_IWUSR);
+		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 		if (fd == -1)
 		{
 			return (-1);
 		}
 
-	bytesWrite = write(fd, text_content, sizeof(char) + 1);
-		if (bytesWrite == -1)
+	bytesToWrite = write(fd, text_content, sizeof(char) + 1);
+		if (bytesToWrite == -1)
 		{
 			close(fd);
 			return (-1);
@@ -33,7 +33,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	else
 	{
-		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC | S_IRUSR | S_IWUSR);
+		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 		if (fd == -1)
 		{
 			return (-1);
