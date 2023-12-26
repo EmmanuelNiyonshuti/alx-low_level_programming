8d26d04c41b06c274a6f4b84e04533a610625543
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2023-12-25 19:02:29 -0800

    modified the loop condition inside 2-strchar.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
- * _strchr - Locates the first occurrence of the character @c in the string @s.
+ * _strchr - Locates a character in a string.
  *
- * @s: A pointer to the string to search.
- * @c: The character to locate within the string @s.
+ * @s:  pointer to the string.
+ * @c: The character to locate.
  *
- * Return: A pointer to the first occurrence of @c in @s,
- * or NULL if @c is not found.
+ * Return: A pointer to the first occurrence of c in s,
+ * or NULL if c is not found.
  */
 char *_strchr(char *s, char c)
 {
 	int k;
 
-	for (k = 0; s[k] >= '\0'; k++)
+	for (k = 0; s[k] != '\0'; k++)
 	{
 	if (s[k] == c)
 	return (s + k);
