bc22f23c873a6a52e60e8b3abc693bb6739bca5a
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-01-11 14:23:39 -0800

    all elements of a list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * dlistint_len - Returns the number of elements in a linked dlistint_t list.
+ * @h: Pointer to the first node in a list.
+ *
+ * Return: The number of elements in list.
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int elements_num = 0;
+
+	while (h != NULL)
+	{
+		elements_num = h->n;
+		h = h->next;
+		elements_num++;
+	}
+	return (elements_num);
+}
