7526e8d1d6100cce407ca349272eca39ddd80ef3
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-01-12 07:38:05 -0800

    check 5 error

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -4,7 +4,6 @@
  *@h: Points to a pointer that points to a header.
  *@idx:The index of the list where the new node should be added
  *@n: an integer data
- *
  *Return: The address of the new node, or NULL if it failed
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
@@ -16,33 +15,38 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
 		return (NULL);
-
+	/*Set data for the new node*/
 	new_node->n = n;
-	new_node->next = NULL;
-	new_node->prev = NULL;
+
+	/*Check if the list is empty and the index is 0*/
 	if (*h == NULL && idx == 0)
 	{
 		*h = new_node;
 		return (new_node);
 	}
+	/*Traverse the list to find the insertion point*/
 	while (current != NULL && nodes_num < idx)
 	{
 		current = current->next;
 		nodes_num++;
 	}
+	/*Check if the index is out of bounds*/
 	if (nodes_num < idx)
 	{
 		free(new_node);
 		return (NULL);
 	}
-	else if (current->prev != NULL)
+	/* Update pointers to insert the new node at the specified index*/
+	if (current->prev != NULL)
 		current->prev->next = new_node;
 	else
 		*h = new_node;
-
+	/*Complete the linkage of the new node*/
 	new_node->prev = current->prev;
 	new_node->next = current;
 	current->prev = new_node;
 
+	/*Return the address of the new node*/
 	return (new_node);
 }
+
