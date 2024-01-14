8e584bc25bbe785aba58cfb706c05c9475609c41
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-01-14 03:02:26 -0800

    inserting a node at a given index reviewed my code

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,41 +12,31 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *current = *h;
 	unsigned int nodes_num = 0;
 
-	new_node = malloc(sizeof(dlistint_t));
+	new = malloc(sizeof(dlistint_t));
 	if (!new_node)
 		return (NULL);
-	/*Set data for the new node*/
 	new_node->n = n;
+	new_node->next = NULL;
+	new_node->prev = NULL;
 
-	/*Check if the list is empty and the index is 0*/
-	if (*h == NULL && idx == 0)
-	{
+	if (*h == NULL)
 		*h = new_node;
-		return (new_node);
-	}
-	/*Traverse the list to find the insertion point*/
-	while (current != NULL && nodes_num < idx)
-	{
-		current = current->next;
-		nodes_num++;
-	}
-	/*Check if the index is out of bounds*/
-	if (nodes_num < idx)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	/* Update pointers to insert the new node at the specified index*/
-	if (current->prev != NULL)
-		current->prev->next = new_node;
 	else
-		*h = new_node;
-	/*Complete the linkage of the new node*/
-	new_node->prev = current->prev;
-	new_node->next = current;
-	current->prev = new_node;
+	{
+		while (current != NULL && current->next != NULL)
+		{
+			if (nodes_num == idx)
+			{
+				current->next->prev = new_node;
+				new_node->next = current->next;
+				new_node->prev = current;
+				current->next = new_node;
 
-	/*Return the address of the new node*/
+				return (new_node);
+			}
+			current = current->next;
+			nodes_num++;
+		}
+	}
 	return (new_node);
 }
-
