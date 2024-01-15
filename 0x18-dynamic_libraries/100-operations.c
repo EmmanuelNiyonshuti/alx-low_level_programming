8b1895cb1aea2effa158392ce2ee44c6c7c79eaa
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-01-15 11:14:25 -0800

    2. Let's call C functions from Python

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,18 +1,44 @@
 #include <stdio.h>
+/**
+ * add - adds two integers.
+ * @a: int
+ * @b: int
+ *
+ * Return: sum
+ */
 int add(int a, int b)
 {
 	return (a + b);
 }
-
+/**
+ * sub - substracts two integers.
+ * @a: int
+ * @b: int
+ *
+ * Return: substraction
+ */
 int sub(int a, int b)
 {
 	return (a - b);
 }
-
+/**
+ * mul - multiply two integers.
+ * @a: int
+ * @b: int
+ *
+ * Return: product
+ */
 int mul(int a, int b)
 {
 	return (a * b);
 }
+/**
+ * div - divide two integers
+ *@a: int
+ *@b: int
+ *
+ * Return: quotient
+ */
 int div(int a, int b)
 {
 	if (b == 0)
@@ -20,6 +46,13 @@ int div(int a, int b)
 
 	return (a / b);
 }
+/**
+ * mod - computes module of two integers.
+ *@a: an integer
+ *@b: an int
+ *
+ * Return: Remainder
+ */
 int mod(int a, int b)
 {
 	if (b == 0)
