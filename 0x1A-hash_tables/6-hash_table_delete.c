198bd0ea1d728c4f92218b928942a6cc2f65c247
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-02-18 17:54:46 +0200

    Adhering to betty coding standards

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -7,29 +7,28 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-	unsigned long int i =0;
+	unsigned long int i = 0;
 	hash_node_t *current;
 	hash_node_t *temp;
 
-	if (ht == NULL)
-		return;
-
-	/*Deallocates memory for each node in each linked list*/
-	for (i = 0; i < ht->size; ++i)
+	if (ht != NULL)
 	{
-		current = ht->array[i];
-		while (current != NULL)
+		/*Deallocates memory for each node in each linked list*/
+		for (i = 0; i < ht->size; ++i)
 		{
-			temp = current;
-			current = current->next;
-			free(temp->key);
-			free(temp->value);
-			free(temp);
+			current = ht->array[i];
+			while (current != NULL)
+			{
+				temp = current;
+				current = current->next;
+				free(temp->key);
+				free(temp->value);
+				free(temp);
+			}
 		}
+		/*Deallocates memory for the array itself*/
+		free(ht->array);
+		/*Deallocates memory for the hash table structure*/
+		free(ht);
 	}
-	/*Deallocates memory for the array itself*/
-	free(ht->array);
-
-	/*Deallocates memory for the hash table structure*/
-	free(ht);
 }
