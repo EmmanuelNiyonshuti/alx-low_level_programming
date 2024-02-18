a3b54ca29b079f742d1d7e143faa95d1120372d2
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-02-18 03:50:18 +0200

    created hash table and modified README.md

--- 0x1A-hash_tables/README.md
+++ 0x1A-hash_tables/README.md
@@ -1,2 +1,4 @@
-This Directory focus on implementing a hash table data structure in C.
-it will be updated as I progress to implementing it.
+This Directory focus on Hash Table data structure. Hash Table is a data structure that maps keys to values.
+Hash Tables are the underlying data structure used to implement dictionaries or associative arrays.
+They use hash function to compute an index or position in an array where the values associated with the key should be stored.
+In this directory I will implement a hash table data structure in C language.
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,16 +1,16 @@
 #ifndef HASH_TABLES_H
 #define HASH_TABLES_H
 
-
-
+#include <stdlib.h>
+#include <string.h>
 /**
- * struct hash_node_s - Node of a hash table
+  *struct hash_node_s - Node of a hash table
  *
  * @key: The key, string
  * The key is unique in the HashTable
  * @value: The value corresponding to a key
  * @next: A pointer to the next node of the List
- */
+*/
 typedef struct hash_node_s
 {
 	char *key;
@@ -32,8 +32,8 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 
-
-
-
-
-#endif /*HASH_TABLES_H*/
+hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+#endif/*HASH_TABLES*/
