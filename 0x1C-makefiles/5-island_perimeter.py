c945b710bf8a38314203eed5c0fdc86dab56f20a
NIYONSHUTI Emmanuel <nemmy0257@gmail.com>
2024-04-04 10:12:55 +0200

    grid

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -25,17 +25,18 @@ def island_perimeter(grid):
 
     for i in range(rows):
         for j in range(columns):
-            #Check if the cell represents land
+            """Check if the cell represents land"""
 
             if grid[i][j] == 1:
                 p += 4
-                 # Check if there's a land cell to the left
-                 #and above the current cell
+                """Check if there's a land cell to the left
+                 and above the current cell"""
 
                 if i > 0 and grid[i - 1][j] == 1:
                     p -= 2
                 if j > 0 and grid[i][j - 1] == 1:
                     p -= 2
     return p
+
 
 """Time complexity Proportional to the grid size"""
